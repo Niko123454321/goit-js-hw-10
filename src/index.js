1c6ecd7cc2e7105f9a37a9313e8e53c224b68879
Niko123454321 <kvbsnsk@gmail.com>
2022-12-07 02:08:06 +0200

    start files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('faina_super_dog');
+
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './css/styles.css';
+// import cardTp1 from './templates/countries-card.hbs';
+
+const DEBOUNCE_DELAY = 300;
