7b64bc1b83f5794033a221f007d051092cd292b3
Niko123454321 <kvbsnsk@gmail.com>
2022-12-08 12:08:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,13 +17,11 @@ refs.inputBox.addEventListener('input', debounce(onSubmit, DEBOUNCE_DELAY));
 
 let items = '';
 
-function onSubmit(params) {
+function onSubmit() {
   let box = refs.inputBox.value.trim();
-  console.log(box);
 
   if (box === '') {
-    refs.countryInfo.innerHTML = '';
-    refs.countryList.innerHTML = '';
+    clear();
     return;
   }
 
@@ -37,51 +35,18 @@ function onSubmit(params) {
     .then(countrie => {
       items = countrie;
 
-      if (items.length >= 10) {
-        console.log('больше десяти совпадений');
-        return Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      } else if (items.length > 2) {
-        console.log('больше двух совпадений');
-        refs.countryList.innerHTML = '';
-        return items.forEach(element => {
-          refs.countryList.insertAdjacentHTML(
-            'beforeend',
-            `<li class="country-list__item">
-    <img class="country-list__image" src="${element.flags.png}" alt="${element.name.official}" width='25' >
-    <p class="country-list_name">${element.name.official}</p>
-</li>`
-          );
-        });
-      } else if ((items.length = 1)) {
-        console.log('нашлось одно совпадене');
-
-        refs.countryList.innerHTML = '';
-
-        return items.forEach(element => {
-          refs.countryList.insertAdjacentHTML(
-            'beforeend',
-            `<li>
-    <img class="country-list__image" src="${element.flags.png}" alt="${
-              element.name.official
-            }" heigth='20' width='35' >
-    <p class="name_official">${element.name.official}</p>
-    <p ><span class="name_text">Capital:
-  </span> ${element.capital}</p>
-    <p ><span class="name_text">Population: 
-  </span>${element.population}</p>
-    <p ><span class="name_text">Languages: 
-  </span>${Object.values(element.languages)}</p>
-  </li>`
-          );
-        });
+      if (items.length > 10) {
+        clear();
+        return errInfo();
+      } else if (items.length >= 2) {
+        countrysMarkup();
+      } else if (items.length === 1) {
+        return oneCountryMarkup();
       }
     })
 
     .catch(error => {
-      console.log(error);
-      return Notify.failure('Oops, there is no country with that name');
+      return errFail();
     });
 }
 
@@ -90,3 +55,51 @@ function onSubmit(params) {
 // population - населення
 // flags.svg - посилання на зображення прапора
 // languages - масив мов
+
+function clear() {
+  refs.countryList.innerHTML = '';
+}
+
+function oneCountryMarkup() {
+  clear();
+  items.forEach(element => {
+    refs.countryList.insertAdjacentHTML(
+      'beforeend',
+      `<li>
+    <img class="country-list__image" src="${element.flags.png}" alt="${
+        element.name.official
+      }" heigth='20' width='35' >
+    <p class="name_official">${element.name.official}</p>
+    <p ><span class="name_text">Capital:
+  </span> ${element.capital}</p>
+    <p ><span class="name_text">Population: 
+  </span>${element.population}</p>
+    <p ><span class="name_text">Languages: 
+  </span>${Object.values(element.languages)}</p>
+  </li>`
+    );
+  });
+}
+
+function countrysMarkup() {
+  clear();
+  return items.forEach(element => {
+    refs.countryList.insertAdjacentHTML(
+      'beforeend',
+      `<li class="country-list__item">
+    <img class="country-list__image" src="${element.flags.png}" alt="${element.name.official}" width='25' >
+    <p class="country-list_name">${element.name.official}</p>
+</li>`
+    );
+  });
+}
+
+function errInfo() {
+  clear();
+  Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+function errFail() {
+  clear();
+  Notify.failure('Oops, there is no country with that name');
+}
